f6b9b030858b3a88b8f6c208253ba3704c04d416
javahometech <hari.kammana@gmail.com>
2024-07-16 07:37:59 +0530

    Update nexusUpload.groovy

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -8,5 +8,8 @@ def call(){
   if(version.endsWith("SNAPSHOT")){
        repoName = "${repoName}-snapshot"
   }
-  nexusArtifactUploader artifacts: [[artifactId: artifactId, classifier: '', file: "target/${artifactId}.war", type: 'war']], credentialsId: 'nexus3', groupId: groupId, nexusUrl: '172.31.15.204:8081', nexusVersion: 'nexus3', protocol: 'http', repository: "${repoName}", version: version
+  nexusArtifactUploader artifacts: [[artifactId: artifactId, classifier: '', file: "target/${artifactId}.war", type: 'war']], 
+                        credentialsId: 'nexus3', groupId: groupId, nexusUrl: '172.31.15.204:8081', 
+                        nexusVersion: 'nexus3', protocol: 
+                        'http', repository: "${repoName}", version: version
 }
